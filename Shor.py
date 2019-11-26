9bab26c6bdbb59ad1f6298ef634c9cc81f58ab1e
Paul Pesnell <paul.pesnell@gmail.com>
2019-11-26 17:55:38 -0500

    Shors Stuff

--- Shor.py
+++ Shor.py
@@ -1,9 +1,16 @@
 import math
+import random
 
 from qiskit import Aer
 from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
 from qiskit import execute
 from qiskit.tools.visualization import plot_histogram, circuit_drawer
+
+
+def main():
+    N = 15
+    x, r, p, q = shor_quantum(N)
+    print("semiprime N = ", N, ", coprime x = ", x, ", period r = ", r, ", prime factors = ", p, " and ", q, sep="")
 
 
 # qc = quantum circuit, qr = quantum register, cr = classical register, a = 2, 7, 8, 11 or 13
@@ -127,15 +134,42 @@ def circuit_11period15(qc, qr, cr):
     qc.measure(qr[4], cr[2])
 
 
-q = QuantumRegister(5, 'q')
-c = ClassicalRegister(5, 'c')
+def shor_quantum(n):
+    q = QuantumRegister(5, 'q')
+    c = ClassicalRegister(5, 'c')
 
-shor = QuantumCircuit(q, c)
-circuit_aperiod15(shor, q, c, 7)
+    shor = QuantumCircuit(q, c)
+    best = 1
+    x = 0
 
-backend = Aer.get_backend('qasm_simulator')
-sim_job = execute([shor], backend)
-sim_result = sim_job.result()
-sim_data = sim_result.get_counts(shor)
-print(sim_data)
-plot_histogram(sim_data)
+    while best % 2 != 0:
+        x = random.randint(2, n - 1)
+        if math.gcd(x, n) != 1:
+            return x, 0, math.gcd(x, n), n / math.gcd(x, n)
+        circuit_aperiod15(shor, q, c, 7)
+
+        backend = Aer.get_backend('qasm_simulator')
+        sim_job = execute([shor], backend)
+        sim_result = sim_job.result()
+        sim_data = sim_result.get_counts(shor)
+        print(sim_data)
+        best = best_result_number(sim_data)
+        if x ** (best / 2) == 1 or x ** (best / 2) == -1:
+            best = 1
+    p = math.gcd(x ** (int(best / 2)) + 1, 15)
+    q = math.gcd(x ** (int(best / 2)) - 1, 15)
+    return x, best, p, q
+
+
+def best_result_number(data):
+    highest_hits = 0
+    best_value = 0
+    for key in data:
+        if data[key] > highest_hits:
+            highest_hits = data[key]
+            best_value = key
+    return int(best_value)
+
+
+if __name__ == "__main__":
+    main()
